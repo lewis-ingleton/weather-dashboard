64c62d7d2375d29d23def5b8b0681c7a0a0b5f90
Lewis Ingleton <lewis.ingleton1@.com>
2023-02-06 22:55:51 +0000

    5-day forecast loop success

--- js.folder/script.js
+++ js.folder/script.js
@@ -1,203 +1,60 @@
-
-
+// global variables 
 const key = '5d4862fa13354a7e3c01431d3829c345';
-let city = 'london';
-const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=en&limit=4&appid=' + key;
-console.log(currentWeatherURL)
 
 
+// Show today's date and time 
+let currentDate = moment();
+$(document).ready(function () {
+    $('#currentDay').text(currentDate.format('Do MMMM YYYY, h:mm a'));
+});
 
+// Get current weather
+$('#search-button').on('click', function (event) {
+    let city = $('#search-input').val();
+    event.preventDefault();
+    const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=en&limit=4&appid=' + key;
+    console.log(currentWeatherURL)
 
+    $.ajax({
+        url: currentWeatherURL,
+        method: "GET"
+    }).then(function (response) {
+        // Fetch data from API 
+        let city = (response.name + ', ' + response.sys.country)
+        let currentTemp = Math.round((response.main.temp)) + '°C';
+        let currentWind = (response.wind.speed) + ' m/s';
+        let currentHumidity = (response.main.humidity) + '%';
 
-// Get weather now
-$.ajax({
-    url: currentWeatherURL,
-    method: "GET"
-}).then(function (response) {
-
-    // SET UP MOMENT.JS FOR TODAY'S DATE AND THEN ADD TO VARIABLE 
-    // let todaysDate = 
-    let city = (response.name + ', ' + response.sys.country)
-    let currentTemp = Math.round((response.main.temp)) + '°C';
-    let currentWind = (response.wind.speed) + ' m/s';
-    let currentHumidity = (response.main.humidity) + '%';
-
-    // Inner text + append
-    $('#todays-weather').text('Current weather for ' + city);
-    $('#current-weather-card').append('<p>Temperature: ' + currentTemp + '</p>');
-    $('#current-weather-card').append('<p>Wind speed: ' + currentWind + '</p>');
-    $('#current-weather-card').append('<p>Humidity: ' + currentHumidity + '</p>');
+        // Inner text + append
+        $('#todays-weather').text('Current weather for ' + city);
+        $('#current-weather-card').append('<p>Temperature: ' + currentTemp + '</p>');
+        $('#current-weather-card').append('<p>Wind speed: ' + currentWind + '</p>');
+        $('#current-weather-card').append('<p>Humidity: ' + currentHumidity + '</p>');
+    })
 })
 
-//// APPEND TO currentWeatherCard
-const forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&lang=en&limit=3&appid=' + key;
-console.log(forecastURL)
-// Get forecast
-$.ajax({
-    url: forecastURL,
-    method: "GET"
-}).then(function (oneDay) {
-    // Forecast data from API
-    let getDate = (oneDay.list[8].dt_txt)
-    console.log(getDate)
-    let convertedDate = moment(getDate).format('DD MMM YYYY');
-    console.log(convertedDate)
+// Get forecasted weather 
+$('#search-button').on('click', function (event) {
+    let city = $('#search-input').val();
+    event.preventDefault();
+    const forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&lang=en&limit=3&appid=' + key;
+    console.log(forecastURL)
 
-    let temp = Math.round((oneDay.list[8].main.temp)) + '°C'
-    console.log(temp)
-    let wind = (oneDay.list[8].wind.speed) + ' m/s'
-    console.log(wind)
-    let humidity = (oneDay.list[8].main.humidity) + '%'
-    console.log(humidity)
-
-    // Inner text + append 
-    $('.forecast-date').text(convertedDate);
-    $('#card-body-1').append('<p>Temperature: ' + temp + '</p>');
-    $('#card-body-1').append('<p>Wind speed: ' + wind + '</p>');
-    $('#card-body-1').append('<p>Humidity: ' + humidity + '</p>');
-
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    }).then(function (response) {
+        for (i = 7; i < response.list.length; i += 8) {
+            let getDate = (response.list[i].dt_txt)
+            let convertedDate = moment(getDate).format('DD MMM YYYY');
+            let temp = Math.round((response.list[i].main.temp)) + '°C'
+            let wind = (response.list[i].wind.speed) + ' m/s'
+            let humidity = (response.list[i].main.humidity) + '%'
+            $('.forecast-date').text(convertedDate);
+            $('.forecast-card').append('<p>Temperature: ' + temp + '</p>');
+            $('.forecast-card').append('<p>Wind speed: ' + wind + '</p>');
+            $('.forecast-card').append('<p>Humidity: ' + humidity + '</p>');
+            console.log(convertedDate)
+        }
+    })
 })
-
-
-
-// 48 HOURS IN FORECAST 
-// .then(function (twoDay) {
-//     // Forecast data from API
-//     let getDate = (twoDay.list[16].dt_txt)
-//     console.log(getDate)
-
-//     let temp = Math.round((twoDay.list[16].main.temp)) + '°C'
-//     console.log(temp)
-//     let wind = (twoDay.list[16].wind.speed) + ' m/s'
-//     console.log(wind)
-//     let humidity = (twoDay.list[16].main.humidity) + '%'
-//     console.log(humidity)
-
-//     // Inner text + append 
-//     $('.forecast-date').text(getDate);
-//     $('#card-body-2').append('<p>Temperature: ' + temp + '</p>');
-//     $('#card-body-2').append('<p>Wind speed: ' + wind + '</p>');
-//     $('#card-body-2').append('<p>Humidity: ' + humidity + '</p>');
-// })
-
-
-// 72 HOURS+ FORECAST
-// .then(function (threeDays) {
-//     let temp = Math.round((threeDays.list[24].main.temp)) + '°C'
-//     console.log(temp)
-//     let wind = (threeDays.list[24].wind.speed) + ' m/s'
-//     console.log(wind)
-//     let humidity = (threeDays.list[24].main.humidity) + '%'
-//     console.log(humidity)
-// }).then(function (fourDays) {
-//     let temp = Math.round((fourDays.list[32].main.temp)) + '°C'
-//     console.log(temp)
-//     let wind = (fourDays.list[32].wind.speed) + ' m/s'
-//     console.log(wind)
-//     let humidity = (fourDays.list[32].main.humidity) + '%'
-//     console.log(humidity)
-// }).then(function (fiveDays) {
-//     let temp = Math.round((fiveDays.list[40].main.temp)) + '°C'
-//     console.log(temp)
-//     let wind = (fiveDays.list[40].wind.speed) + ' m/s'
-//     console.log(wind)
-//     let humidity = (fiveDays.list[40].main.humidity) + '%'
-//     console.log(humidity)
-// })
-
-// 1 day in advance
-// list[8]
-// 2 day in advance
-// list[16]
-// 3 day in advance
-// list[24]
-// 4 day in advance
-// list[32]
-// 5 day in advance
-// list[40]
-
-
-
-// // get currentWeather
-// const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=en&limit=3&appid=' + key;
-// .ajax({
-//     url: currentWeatherURL,
-//     url: forecastURL,
-//     method: "GET"
-// }).then(function (response) {
-//     // let today = Math.round((response.main.temp))
-//     // console.log(today + ' hello')
-// })
-
-
-
-
-
-
-
-//list.
-
-
-// $.ajax({
-//     url: cityURL,
-//     method: "GET"
-// }).then(function (response) {
-//     let convertedLat = (response[0].lat);
-//     let convertedLon = (response[0].lon);
-//     console.log(convertedLat)
-//     console.log(convertedLon)
-//     let units = 'metric';
-//     let lang = 'en';
-//     const queryURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + convertedLat + '&lon=' + convertedLon + '&units=' + units + '&lang=' + lang + '&appid=' + key;
-//     console.log(queryURL)
-// })
-
-
-
-
-// if user searches london > 
-// find lat and lon > 
-// use the lat and lon data from 
-
-
-
-
-
-// then display weather based on lat and long from searching city name
-
-// const weatherURL = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=' +key;
-// fun
-
-// const fetchForecast = () => {
-//     let lang = 'en';
-//     let units = 'metric';
-//     let url = 'api.openweathermap.org/data/2.5/forecast?';
-// }
-
-
-
-// Geo to city converter 
-// http://api.openweathermap.org/geo/1.0/direct? 
-// Example: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=${key}
-
-// const fetchTodayWeather = () => {
-
-
-// }
-
-// const showWeather = () => {
-
-
-// }
-
-
-
-
-// #today = today's weather 
-
-
-// #forecast = 5-day forecast 
-
-
-// #search-button = button to call api 
-// could think about adding 'get current location' button. using geo-targeted location
